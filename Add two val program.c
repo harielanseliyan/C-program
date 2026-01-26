7d7f7972d4bac7bc08aa61081ba9fee0fbed0a65
harielanseliyan <harielanseliyan@gmail.com>
2026-01-26 09:22:34 +0530

    Add files via upload
    Add two variable

---
+++ Add two val program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a=10;
+    int b=5;
+    int c=19;
+    int d=11;
+    int sum=a+b;
+    printf("%d \n",sum);
+    int tot=c+d;
+    printf("%d",tot);
+    
+    return 0;
+}
