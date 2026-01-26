e36a002c64ce6edaa92a52e559e69f4b88c4aaaf
harielanseliyan <harielanseliyan@gmail.com>
2026-01-26 09:23:14 +0530

    Add files via upload
    Subtract two variables

---
+++ Sub two program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a=10;
+    int b=5;
+    int c=15;
+    int d=5;
+    int ans=a-b;
+    printf("%d \n",ans);
+    int ans1=c-d;
+    printf("%d",ans1);
+    
+    return 0;
+}
