027b90539e06bf47596e168822bcc86fb41fca1d
harielanseliyan <harielanseliyan@gmail.com>
2026-01-26 09:19:33 +0530

    Add files via upload
    declare two int variable

---
+++ Dec Two int program.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num=5;
+    int num2=15;
+    int num3=20;
+    int num4=40;
+    printf("%d %d \n",num,num2);
+    printf("%d %d",num3,num4);
+    return 0;
+}
