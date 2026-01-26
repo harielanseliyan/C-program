97936cc7bf9386b7c3b00dd79e3f717ced3c8df6
harielanseliyan <harielanseliyan@gmail.com>
2026-01-26 09:23:52 +0530

    Add files via upload
    Multiple Two Variables

---
+++ Mul Two num program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a=10;
+    int b=2;
+    int c=10;
+    int d=5;
+    int ans=a*b;
+    printf("%d \n",ans);
+    int ans1=c*d;
+    printf("%d",ans1);
+    
+    return 0;
+}
