7cf355a02143d659d8fc37fb246c8a6f3940df55
harielanseliyan <harielanseliyan@gmail.com>
2026-01-26 09:24:37 +0530

    Add files via upload
    Divide two variables

---
+++ Div Two num program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a=8;
+    int b=2;
+    int c=20;
+    int d=4;
+    int ans=a/b;
+    printf("%d \n",ans);
+    int ans1=c/d;
+    printf("%d",ans1);
+    
+    return 0;
+}
