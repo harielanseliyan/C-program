78f3551c9286aff4d4af8f6f1d5382f84c5a3124
harielanseliyan <harielanseliyan@gmail.com>
2026-01-28 00:23:23 +0530

    Add files via upload

---
+++ vowel or consonant.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    ch = tolower(ch);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+        printf("Vowel");
+    }
+    else{
+        printf("Consonant");
+    }
+    return 0;
+}
