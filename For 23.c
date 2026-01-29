9d45cb769c45025c8d0be92622b761ebac716cdb
harielanseliyan <harielanseliyan@gmail.com>
2026-01-29 17:09:27 +0530

    Add files via upload

---
+++ For 23.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, prime = 1;
+    scanf("%d", &n);
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            prime = 0;
+            break;
+        }
+    }
+    if (prime == 1)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
